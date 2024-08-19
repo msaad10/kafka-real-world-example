4fa27061e1f4759726fb12dfb8ddf98ee8d71f08
msaad10 <saad.muhammad@tenpearls.com>
2024-08-19 17:39:34 +0500

    Initialize repo

---
+++ src/test/java/com/example/kafkarealworldexample/KafkarealworldexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkarealworldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkarealworldexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
